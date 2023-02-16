c3a8fc313f60437a2a600d232305cf403a89a033
pvatsal237 <pvatsal237@gmail.com>
2023-02-16 12:14:09 -0500

    GitHub session

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Shopper's Stop</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
